2073c95f251ed33a0359a47358a8d2569c2afa50
eliot guihal <eliot.guihal@ynov.com>
2024-10-15 15:00:27 +0100

    Knife routes + model

--- src/models/knife.ts
+++ src/models/knife.ts
@@ -1,5 +1,5 @@
 import { Schema, model, Document } from 'mongoose';
-import {Handler} from "express";
+
 const enum Handle {
     Wood = "wood",
     Horn = "horn",
@@ -20,14 +20,24 @@ interface knife extends Document {
     name: string;
     image: string;
     handle: Handle;
-    blade: number;
-    price: number;
+    blade: string;
     sharpness: number;
+    price: number;
     durability: number;
+    weight: number;
+    length: number;
 }
 
 const knifeSchema = new Schema<knife>({
-   //TODO
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    handle: { type: String, required: true },
+    blade: { type: String, required: true },
+    sharpness: { type: Number, default: 5 },
+    durability: { type: Number, default: 0 },
+    weight: { type: Number, default: 0 },
+    length: { type: Number, default: 0 },
+    price: { type: Number, required: true },
 });
 
 export default model<knife>('Knife', knifeSchema);
