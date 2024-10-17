3cfb16e29941f373601da2c1d7522fac890061e1
Rayane Guemmoud <guemmoudrayane03@gmail.com>
2024-10-17 12:29:24 +0200

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import mongoose from './db';
 import router from './routes/knifeRoutes';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/knives', router);
 
